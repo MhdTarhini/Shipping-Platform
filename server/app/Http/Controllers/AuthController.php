0ddc4c5687e425d1dc528796438e056fba13b3b8
MhdTarhini <mohamedtarhini95@gmail.com>
2023-11-03 19:01:12 +0200

    Server <> Implement Auth Methodes + Api Routes

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -2,9 +2,96 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+     public function unauthorized(Request $request){
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Unauthorized',
+        ], 200);
+    }
+
+    public function profile(Request $request){
+        return response()->json([
+            'status' => 'success',
+            'data' => Auth::user(),
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $userModel = User::find($user->id);
+        $userModel->number_of_visit = $userModel->number_of_visit + 1;
+        $userModel->save();
+        return response()->json([
+                'status' => 'success',
+                'user' => $user,
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer',
+                ]
+            ]);
+
+    }
+
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'string',
+        ]);
+
+        $user = new User; 
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        $token = Auth::login($user);
+        $user->token = $token;
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $user
+        ]);
+    }
+
+    public function logout(){
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    public function refresh() {
+        $user = Auth::user();
+        $user->token = Auth::refresh();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $user
+        ]);
+    }
+
 }
