6c2d4befdf6cf1c2e4e70267970d1336d2391a96
MhdTarhini <mohamedtarhini95@gmail.com>
2023-11-03 19:26:18 +0200

    Client <> Create Input Component

---
+++ client/src/components/input/input.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./index.css";
+
+function Input({ onchange, label, name, type, value }) {
+  const [showLabel, setShowLabel] = useState(false);
+
+  const isEmpty = (e) => {
+    if (e.target.value == "") {
+      setShowLabel(false);
+    } else {
+      setShowLabel(true);
+    }
+  };
+
+  return (
+    <div className="flex column input ">
+      <label
+        htmlFor={name}
+        className={
+          showLabel || value ? " label label-class" : " label none-opacity"
+        }>
+        {label}
+      </label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={value ? value : label}
+        onChange={(e) => {
+          onchange(e);
+          isEmpty(e);
+        }}
+      />
+    </div>
+  );
+}
+
+export default Input;
