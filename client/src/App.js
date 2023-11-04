4f3c463aa972841dc96b9e81e15119cb3c0b5754
MhdTarhini <mohamedtarhini95@gmail.com>
2023-11-04 21:41:10 +0200

    Client <> Create User Slice

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
 import HomeLayout from "./homeLayout/homeLayout";
+import { Provider } from "react-redux";
+import { store } from "./rkt/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<HomeLayout />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<HomeLayout />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Input from "../../components/input/input";
 import "./index.css";
 import { useNavigate } from "react-router-dom";
